dfb3cf70c86bbbc65615df5fc49cafc3d2368bef
Taeuk Kim <twotowers7@gmail.com>
2024-03-04 17:25:12 +0900

    ・기본 커서 변경(임시) ・공통 오브젝트에 사용할 예정인 스크립트 추가

---
+++ IronBreaker/Assets/Scripts/Object/CommonObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+//오브젝트의 공통적인 기능
+public class CommonObject : MonoBehaviour
+{
+  public Texture2D cursorCrosshair;
+  public Texture2D cursorDialog;
+
+  //기본값
+  void Start()
+  {
+    //커서의 디폴트값을 크로스헤어로 한다. 씬에 따라 다를 가능성 있음.
+    Cursor.SetCursor(cursorCrosshair, Vector2.zero, CursorMode.ForceSoftware);
+  }
+
+  //오브젝트에 마우스가 들어왔을때
+  void OnMouseEnter()
+  {
+    Cursor.SetCursor(cursorDialog, Vector2.zero, CursorMode.ForceSoftware);
+  }
+
+  //오브젝트에서 마우스가 나갔을때
+  void OnMouseExit()
+  {
+    Cursor.SetCursor(cursorCrosshair, Vector2.zero, CursorMode.ForceSoftware);
+  }
+}
