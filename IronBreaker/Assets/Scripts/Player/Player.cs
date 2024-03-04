7ce33411525e2c0da5425caa8e7c85f4c9e80e8c
Taeuk Kim <twotowers7@gmail.com>
2024-03-04 20:04:12 +0900

    ・Player와 PlayerController 스크립트로 역할을 나눔 Player : 플레이어의 데이터   Controller : 입력 및 컨트롤 ・crosshair.png 적용

--- IronBreaker/Assets/Scripts/Player/Player.cs
+++ IronBreaker/Assets/Scripts/Player/Player.cs
@@ -1,60 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.InputSystem;
+﻿using UnityEngine;
 
 
-//CharaterController2D 참고
-public enum GroundType
-{
-  NONE,
-  SOFT,
-  HARD,
-}
-
+//이 스크립트는 "플레이어의 데이터"와 관련된 내용이 들어갑니다.
 public class Player : MonoBehaviour
 {
-  [Header("캐릭터 스테이터스")]
-  public float speed; //테스트용으로했을때 1초에 4개타일을 움직이려면 3.5~4이내의 값이 필요했음.
+  public float MaxHP { get { return maxHP; } }    //최대 체력
+  public float CurrentHP { get { return currentHP; } }    //현재 체력
+  public float MaxStamina { get { return maxStamina; } }    //최대 스태미나
+  public float CurrentStamina { get { return currentStamina; } }
+  public float MaxMagneticPower { get { return maxMP; } }//maxMagneticPower의 약
+  public float CurrentMagneticPower { get { return currentMP; } }
+  public float Defense { get { return defense; } }
+  public float MoveSpeed { get { return moveSpeed; } } //테스트용으로했을때 1초에 4개타일을 움직이려면 3.5~4이내의 값이 필요했음.
 
-  [SerializeField] private Rigidbody2D playerRigid;
+  [Header("플레이어 스테이터스")]
+  [SerializeField] protected float maxHP;
+  [SerializeField] protected float currentHP;
+  [SerializeField] protected float maxStamina;
+  [SerializeField] protected float currentStamina;
+  [SerializeField] protected float maxMP; 
+  [SerializeField] protected float currentMP;
+  [SerializeField] protected float defense;
+  [SerializeField] protected float moveSpeed;
 
-  private Vector2 movementInput;
-
-  void Update()
+  //stat init
+  public void OnUpdateStat(float maxHP, float currentHP, float maxStamina, float currentStamina, float maxMP, float currentMP, float defense, float moveSpeed)
   {
-
-  }
-
-  void FixedUpdate()
-  {
-  }
-
-  //Input System에 의한 이동
-  public void onMove(InputAction.CallbackContext context)
-  {
-    Vector2 input = context.ReadValue<Vector2>();
-    if (input != null)
-    {
-      movementInput = new Vector2(input.x, input.y);
-      playerRigid.velocity = movementInput * speed;
-    }
-  }
-
-  //Input System에 의한 대쉬
-  public void onDash(InputAction.CallbackContext context)
-  {
-    if (context.performed) //Action type이 "button"일 경우 키가 눌렸는지 체크.
-    {
-      //점프 로직
-    }
-  }
-
-  //애니메이션 추후 추가 예정
-  void PlayerAnimation()
-  {
-    //https://www.youtube.com/watch?v=bZVa6C6vRBQ&list=PLO-mt5Iu5TeYfyXsi6kzHK8kfjPvadC5u&index=2&ab_channel=%EA%B3%A8%EB%93%9C%EB%A9%94%ED%83%88
-    //참고
+    this.maxHP = maxHP;
+    this.currentHP = currentHP;
+    this.maxStamina = maxStamina;
+    this.currentStamina = currentStamina;
+    this.maxMP = maxMP;
+    this.currentMP = currentMP;
+    this.defense = defense;
+    this.moveSpeed = moveSpeed;
   }
 
 
---
+++ IronBreaker/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
+
+//이 스크립트에는 "입력 및 컨트롤"에 관한 내용들이 들어갑니다.
+[RequireComponent(typeof(Player))]
+public class PlayerController : MonoBehaviour
+{
+  protected Player player;
+  public Vector2 movementInput { get; private set; }
+
+  [SerializeField] private Rigidbody2D playerRigid;
+  
+
+  void Start()
+  {
+    player = GetComponent<Player>();
+    playerRigid = GetComponent<Rigidbody2D>();
+  }
+
+
+  //Input System에 의한 이동
+  public void onMove(InputAction.CallbackContext context)
+  {
+    Vector2 input = context.ReadValue<Vector2>();
+    if (input != null)
+    {
+      movementInput = new Vector2(input.x, input.y);
+      playerRigid.velocity = movementInput * player.MoveSpeed;
+    }
+  }
+
+
+  //Input System에 의한 공격과 강공격
+  public void onShot(InputAction.CallbackContext context)
+  {
+    if (context.performed)
+    {
+      //차지 공격
+      if (context.interaction is HoldInteraction)
+      {
+
+      }
+      //기본 공격
+      else if (context.interaction is PressInteraction)
+      {
+
+      }
+    }
+  }
+
+  // 마우스 좌표 따기
+  protected Vector2 GetMouseWorldPosition()
+  {
+    Vector2 mousePosition = Mouse.current.position.ReadValue();
+    Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+    Debug.DrawRay(ray.origin, ray.direction * 1000f, Color.red, 5f);  // Raycast 시각화
+
+    if (Physics.Raycast(ray, out RaycastHit HitInfo, Mathf.Infinity))
+    {
+      Vector2 target = HitInfo.point;
+      Vector2 myPosition = new Vector3(transform.position.x, transform.position.y);
+      target.Set(target.x, target.y);
+      return (target - myPosition).normalized;
+    }
+
+    return Vector2.zero;
+  }
+
+
+  //Input System에 의한 대쉬
+  public void onDash(InputAction.CallbackContext context)
+  {
+    if (context.performed) //Action type이 "button"일 경우 키가 눌렸는지 체크.
+    {
+      //점프 로직
+    }
+  }
+
+  //애니메이션 추후 추가 예정
+  void PlayerAnimation()
+  {
+    //https://www.youtube.com/watch?v=bZVa6C6vRBQ&list=PLO-mt5Iu5TeYfyXsi6kzHK8kfjPvadC5u&index=2&ab_channel=%EA%B3%A8%EB%93%9C%EB%A9%94%ED%83%88
+    //참고
+  }
+
+
+}
