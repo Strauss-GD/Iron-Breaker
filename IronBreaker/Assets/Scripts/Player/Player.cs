591f880f6af4703fc41a35e1aa83f10450fa042b
Taeuk Kim <twotowers7@gmail.com>
2024-03-04 16:04:36 +0900

    ・게임내의 모든 이동 프로세서를 Input System(new)를 사용하여 멀티플랫폼에 적용이 쉽도록 변경. ・이동관련 코드를 간략하게 수정. ・플레이어 스피드 설정 (현 테스트용 이미지를 이용한 이동 시 1초에 4타일을 이동하려면 3.5~4의 스피드가 필요) 다음 푸시부터 기존에 유니티에서 사용하던 PlayerSetting의 InputSystem 사용불가합니다.

--- IronBreaker/Assets/Scripts/Player/Player.cs
+++ IronBreaker/Assets/Scripts/Player/Player.cs
@@ -1,37 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+//CharaterController2D 참고
+public enum GroundType
+{
+  NONE,
+  SOFT,
+  HARD,
+}
 
 public class Player : MonoBehaviour
 {
-  //플레이어의 스피드
-  public float Speed;
+  [Header("캐릭터 스테이터스")]
+  public float speed; //테스트용으로했을때 1초에 4개타일을 움직이려면 3.5~4이내의 값이 필요했음.
 
-  [SerializeField] Rigidbody2D rigid;
-  float h, v;
-  bool isHorizonMove;
+  [SerializeField] private Rigidbody2D playerRigid;
+
+  private Vector2 movementInput;
 
   void Update()
   {
-    //이동 값 설정
-    h = Input.GetAxisRaw("Horizontal");
-    v = Input.GetAxisRaw("Vertical");
 
-    //Check Button Down & Up
-    bool hDown = Input.GetButtonDown("Horizontal");
-    bool vDown = Input.GetButtonDown("Vertical");
-    bool hUp = Input.GetButtonUp("Horizontal");
-    bool vUp = Input.GetButtonUp("Vertical");
-
-    //Check Horizontal Move
-    if (hDown || vUp) isHorizonMove = true;
-    else if (vDown || hUp) isHorizonMove = false;
   }
 
   void FixedUpdate()
   {
-    //이동
-    Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
-    rigid.velocity = moveVec * Speed;
   }
+
+  //Input System에 의한 이동
+  public void onMove(InputAction.CallbackContext context)
+  {
+    Vector2 input = context.ReadValue<Vector2>();
+    if (input != null)
+    {
+      movementInput = new Vector2(input.x, input.y);
+      playerRigid.velocity = movementInput * speed;
+    }
+  }
+
+  //Input System에 의한 대쉬
+  public void onDash(InputAction.CallbackContext context)
+  {
+    if (context.performed) //Action type이 "button"일 경우 키가 눌렸는지 체크.
+    {
+      //점프 로직
+    }
+  }
+
+  //애니메이션 추후 추가 예정
+  void PlayerAnimation()
+  {
+    //https://www.youtube.com/watch?v=bZVa6C6vRBQ&list=PLO-mt5Iu5TeYfyXsi6kzHK8kfjPvadC5u&index=2&ab_channel=%EA%B3%A8%EB%93%9C%EB%A9%94%ED%83%88
+    //참고
+  }
+
+
 }
