986e4ebd9ef39cb93b254e47d7017c89d0b4a9c5
Taeuk Kim <twotowers7@gmail.com>
2024-04-03 17:39:44 +0900

    ・다이얼로그 애니메이션 추가 (DOTween으로 변경 예정) ・타이핑 스크립트 및 사운드 추가

--- IronBreaker/Assets/Scripts/GameManager.cs
+++ IronBreaker/Assets/Scripts/GameManager.cs
@@ -9,11 +9,14 @@ public class GameManager : MonoBehaviour
   //외부 스크립트
   public DialogManager DM; //다이얼로그 매니저
   public QuestManager QM;  //퀘스트 매니저
+  public TypingEffect typingEffect;  //타이핑 이펙트 스크립트
 
   //다이얼로그 패널
-  public GameObject dialogPanel;
+  public Animator dialogPanel;
+  public Animator portraitAnim;
   public TextMeshProUGUI dialogText;
   public Image portraitImg;
+  public Sprite beforePortrait;   //초상화 변경 전의 스프라이트
 
   //조사
   public GameObject scanObject;
@@ -28,17 +31,33 @@ public class GameManager : MonoBehaviour
   //대화창 액션
   public void DiaglogAction(GameObject scanObj)
   {
+    //정면에 있는 오브젝트 정보 취득
     scanObject = scanObj;
     ObjectData objData = scanObject.GetComponent<ObjectData>();
     Talk(objData.id, objData.isNpc);
-    dialogPanel.SetActive(isDialogUp);
+    
+    //다이얼로그 패널 활성 비활성화
+    dialogPanel.SetBool("isShow", isDialogUp);
+
+
   }
 
   void Talk(int id, bool isNpc)
   {
     //Set Dialog Data
-    int questDialogIndex = QM.GetQuestDialogIndex(id);
-    string dialogData = DM.GetDiaglog(id + questDialogIndex, dialogIndex);
+    int questDialogIndex = 0;
+    string dialogData = "";
+
+    if (typingEffect.isTyping)
+    {
+      typingEffect.SetMsg("");
+      return;
+    }
+    else
+    {
+      questDialogIndex = QM.GetQuestDialogIndex(id);
+      dialogData = DM.GetDiaglog(id + questDialogIndex, dialogIndex);
+    }
 
     if (dialogData == null) //대화내용이없을시 대화창 닫힘
     {
@@ -50,14 +69,21 @@ public class GameManager : MonoBehaviour
 
     if (isNpc) //NPC
     {
-      dialogText.text = dialogData.Split(':')[0];
+      typingEffect.SetMsg(dialogData.Split(':')[0]);
 
       portraitImg.sprite = DM.GetPortrait(id, int.Parse(dialogData.Split(':')[1])); //int.Parse는 문자열내에 있는 문자가 사실 문자가아닌 int형 숫자라고 강제 형변환시켜주는것.
       portraitImg.color = new Color(1, 1, 1, 1);
+      
+      //초상화 스프라이트가 다를때
+      if (beforePortrait != portraitImg.sprite)
+      {
+        portraitAnim.SetTrigger("doChange");
+        beforePortrait = portraitImg.sprite;
+      }
     }
     else       //오브젝트
     {
-      dialogText.text = dialogData;
+      typingEffect.SetMsg(dialogData);
 
       //초상화 투명
       portraitImg.color = new Color(1, 1, 1, 0);
---
+++ IronBreaker/Assets/Scripts/UI/TypingEffect.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TypingEffect : MonoBehaviour
+{
+  public float CharPerSeconds;        //초당 글자
+  public GameObject EndCursor;      //대화 패널 우측 하단에 나오는 커서
+
+  string targetMsg;
+  [SerializeField] TextMeshProUGUI msgText;
+  [SerializeField] AudioSource audioSource;
+
+  int index;
+  public bool isTyping;    //파이팅이 진행중인가
+
+  public void SetMsg(string msg)
+  {
+    if (isTyping)
+    {
+      msgText.text = targetMsg;
+      CancelInvoke();
+      EffectEnd();
+    }
+    else
+    {
+      targetMsg = msg;
+      EffectStart();
+    }
+  }
+
+  //타이핑 효과 시작
+  void EffectStart()
+  {
+    msgText.text = "";
+    index = 0;
+    EndCursor.SetActive(false);
+
+    isTyping = true;
+
+    Invoke("Effecting", 1 / CharPerSeconds);
+  }
+
+  //타이핑 효과 진행중
+  void Effecting()
+  {
+    //End Effect
+    if(msgText.text == targetMsg)
+    {
+      EffectEnd();
+      return;
+    }
+
+    msgText.text += targetMsg[index];
+    //사운드
+    if (targetMsg[index] != ' ' || targetMsg[index] != ',') audioSource.Play();
+    index++;
+
+    Invoke("Effecting", 1 / CharPerSeconds);
+  }
+
+  //타이핑 효과 끝
+  void EffectEnd()
+  {
+    isTyping = false;
+    EndCursor.SetActive(true);
+  }
+
+}
